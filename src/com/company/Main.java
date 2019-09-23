fefaaffda1672c451591ac00aed99fe89265de14
Tommy Campbell <tjc2187@gmail.com>
2019-09-23 14:29:09 -0500

    completed fibonacci sequence

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,31 @@
 package com.company;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int firstNumber = 0;
+        int secondNumber = 1;
+        int nextNumber;
+        int runTimes;
+        Scanner in;
+
+        do {
+            in = new Scanner(System.in);
+            System.out.println("How many times would you like to run the program?");
+            runTimes = in.nextInt();
+        } while (runTimes < 0);
+
+
+        for (int i = 0; i < runTimes; i++) {
+            System.out.println(firstNumber);
+            nextNumber = firstNumber + secondNumber;
+            firstNumber = secondNumber;
+            secondNumber = nextNumber;
+
+        }
     }
 }
