46a28242132c0a012e2ccf99bcb26d08b80d3e99
Tommy Campbell <tjc2187@gmail.com>
2019-09-23 14:48:00 -0500

    start again without rebooting

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,31 +1,44 @@
 package com.company;
 
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        int firstNumber = 0;
-        int secondNumber = 1;
-        int nextNumber;
-        int runTimes;
         Scanner in;
+        String continues;
 
         do {
+
+            int firstNumber = 0;
+            int secondNumber = 1;
+            int nextNumber;
+            int runTimes;
+
+            do {
+
+                in = new Scanner(System.in);
+                System.out.println("How many times would you like to run the program?");
+                runTimes = in.nextInt();
+
+            } while (runTimes < 0);
+
+            for (int i = 0; i < runTimes; i++) {
+
+                System.out.println(firstNumber);
+                nextNumber = firstNumber + secondNumber;
+                firstNumber = secondNumber;
+                secondNumber = nextNumber;
+
+            }
+
             in = new Scanner(System.in);
-            System.out.println("How many times would you like to run the program?");
-            runTimes = in.nextInt();
-        } while (runTimes < 0);
+            System.out.println("Again? (y/n)");
+            continues = in.next();
 
+        } while (continues.equals("y"));
 
-        for (int i = 0; i < runTimes; i++) {
-            System.out.println(firstNumber);
-            nextNumber = firstNumber + secondNumber;
-            firstNumber = secondNumber;
-            secondNumber = nextNumber;
-
-        }
+        System.out.println("Goodbye.");
     }
 }
